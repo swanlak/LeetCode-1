a12af064ddc50b89ce8345c956b45f5d030a0b69
yotsuba1022 <yotsuba1022@gmail.com>
2017-02-20 11:58:33 +0800

    Implement Fibonacci

---
+++ src/main/java/idv/carl/leetcode/algorithms/easy/fibonacci/Fibonacci.java
@@ -0,0 +1,23 @@
+package idv.carl.leetcode.algorithms.easy.fibonacci;
+
+/**
+ * @author Carl Lu
+ */
+public class Fibonacci {
+
+    public static long fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+
+    public static void main(String args[]) {
+        int n = 10;
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i + ": " + fibonacci(i));
+        }
+    }
+
+}
